f62df019ce04282dd9c46a832ecaa5783b5f7682
Nikita <npankow93@gmail.com>
2023-12-14 21:53:44 +0300

    add code

--- test1/Home_Work_seminar10.py
+++ test1/Home_Work_seminar10.py
@@ -8,4 +8,16 @@ random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
 data.head()
 '''
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+data.head()
 
+encoder = OneHotEncoder(handle_unknown='ignore')
+encoder_df = pd.DataFrame(encoder.fit_transform(data[['whoAmI']]).toarray().astype(int))
+final_df = data.join(encoder_df)
+print(final_df)
